0ef2f553b138553f2fb8eb3e74d3d85466cd4975
Alexander Stefanov <52860159+alexanderStefanovS@users.noreply.github.com>
2019-07-13 18:48:54 +0300

    Add files via upload

---
+++ lesson1_exercise2.cpp
@@ -0,0 +1,20 @@
+﻿//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	double a, b;
+//	
+//	cout << "Enter a: ";
+//	cin >> a;
+//	cout << "Enter b: ";
+//	cin >> b;
+//
+//	double area = a * b;
+//
+//	cout << "The area of the rectangle is: " << area << "\n";
+//
+//	return 0;
+//}
---
+++ lesson1_exercise5.cpp
@@ -0,0 +1,24 @@
+//
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	double area;
+//
+//	cout << "Enter the area of the square: ";
+//	cin >> area;
+//
+//	cout << "The size of a is: " << sqrt(area) << "\n";
+//
+//	double volume;
+//
+//	cout << "Enter the volume of the square: ";
+//	cin >> volume;
+//
+//	cout << "The size of a is: " << pow(volume, 1/3.) << "\n";
+//
+//	return 0;
+//}
