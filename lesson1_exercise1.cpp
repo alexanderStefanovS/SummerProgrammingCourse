d8ec930072e5685e11d5a02cd6b4350486a8a399
Alexander Stefanov <52860159+alexanderStefanovS@users.noreply.github.com>
2019-07-13 18:52:50 +0300

    Add files via upload

---
+++ lesson1_exercise1.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Oh, what,\n\ta happy day!\nOh, yes,\n\twhat a happy day!\n";
+
+	return 0;
+}
---
+++ lesson1_exercise3..cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const double PI = 3.14159265;
+	double r;
+
+	cout << "Enter radius: ";
+	cin >> r;
+
+	double perimeter = PI * 2 * r;
+	double area = PI * r * r;
+
+	cout << "The perimeter of the circle is: " << perimeter << "\n";
+	cout << "The area of the circle is: " << area << "\n";
+
+	return 0;
+}
